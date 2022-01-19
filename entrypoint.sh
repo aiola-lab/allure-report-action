dbd1cd69621569044a8b793dd8243e8c34e81e45
Ilyas Kabdyushev <ilyas.kabdyushev-cs@aiola.com>
2022-01-19 19:24:10 +0300

    fix: Try keep build order

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ REPOSITORY_OWNER_SLASH_NAME=${INPUT_GITHUB_REPO}
 REPOSITORY_NAME=${REPOSITORY_OWNER_SLASH_NAME##*/}
 GITHUB_PAGES_WEBSITE_URL="https://${INPUT_GITHUB_REPO_OWNER}.github.io/${REPOSITORY_NAME}"
 #echo "Github pages url $GITHUB_PAGES_WEBSITE_URL"
+REPORT_NUMBER=${INPUT_GITHUB_RUN_NUM}
 
 if [[ ${INPUT_SUBFOLDER} != '' ]]; then
     INPUT_ALLURE_HISTORY="${INPUT_ALLURE_HISTORY}/${INPUT_SUBFOLDER}"
@@ -18,7 +19,10 @@ if [[ ${INPUT_SUBFOLDER} != '' ]]; then
     mkdir -p ./${INPUT_ALLURE_HISTORY}
     GITHUB_PAGES_WEBSITE_URL="${GITHUB_PAGES_WEBSITE_URL}/${INPUT_SUBFOLDER}"
     echo "NEW github pages url ${GITHUB_PAGES_WEBSITE_URL}"
+    REPORT_NUMBER=$(($(cat ${INPUT_ALLURE_HISTORY}/index.html | sed -nr "s/.*${INPUT_SUBFOLDER}\/([0-9]+)\/.*/\1/p")+1))
 fi
+
+echo "REPORT_NUMBER: ${REPORT_NUMBER}"
 
 COUNT=$( ( ls ./${INPUT_ALLURE_HISTORY} | wc -l ) )
 echo "count folders in allure-history: ${COUNT}"
@@ -35,16 +39,16 @@ if (( COUNT > INPUT_KEEP_REPORTS )); then
 fi
 
 #echo "index.html"
-echo "<!DOCTYPE html><meta charset=\"utf-8\"><meta http-equiv=\"refresh\" content=\"0; URL=${GITHUB_PAGES_WEBSITE_URL}/${INPUT_GITHUB_RUN_NUM}/\">" > ./${INPUT_ALLURE_HISTORY}/index.html # path
+echo "<!DOCTYPE html><meta charset=\"utf-8\"><meta http-equiv=\"refresh\" content=\"0; URL=${GITHUB_PAGES_WEBSITE_URL}/${REPORT_NUMBER}/\">" > ./${INPUT_ALLURE_HISTORY}/index.html # path
 echo "<meta http-equiv=\"Pragma\" content=\"no-cache\"><meta http-equiv=\"Expires\" content=\"0\">" >> ./${INPUT_ALLURE_HISTORY}/index.html
 #cat ./${INPUT_ALLURE_HISTORY}/index.html
 
 #echo "executor.json"
 echo '{"name":"GitHub Actions","type":"github","reportName":"Allure Report with history",' > executor.json
 echo "\"url\":\"${GITHUB_PAGES_WEBSITE_URL}\"," >> executor.json # ???
-echo "\"reportUrl\":\"${GITHUB_PAGES_WEBSITE_URL}/${INPUT_GITHUB_RUN_NUM}/\"," >> executor.json
+echo "\"reportUrl\":\"${GITHUB_PAGES_WEBSITE_URL}/${REPORT_NUMBER}/\"," >> executor.json
 echo "\"buildUrl\":\"https://github.com/${INPUT_GITHUB_REPO}/actions/runs/${INPUT_GITHUB_RUN_ID}\"," >> executor.json
-echo "\"buildName\":\"GitHub Actions Run #${INPUT_GITHUB_RUN_ID}\",\"buildOrder\":\"${INPUT_GITHUB_RUN_NUM}\"}" >> executor.json
+echo "\"buildName\":\"GitHub Actions Run #${INPUT_GITHUB_RUN_ID}\",\"buildOrder\":\"${REPORT_NUMBER}\"}" >> executor.json
 #cat executor.json
 mv ./executor.json ./${INPUT_ALLURE_RESULTS}
 
@@ -61,7 +65,7 @@ allure generate --clean ${INPUT_ALLURE_RESULTS} -o ${INPUT_ALLURE_REPORT}
 #echo "listing report directory ..."
 #ls -l ${INPUT_ALLURE_REPORT}
 
-echo "copy allure-report to ${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_RUN_NUM}"
-cp -r ./${INPUT_ALLURE_REPORT}/. ./${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_RUN_NUM}
+echo "copy allure-report to ${INPUT_ALLURE_HISTORY}/${REPORT_NUMBER}"
+cp -r ./${INPUT_ALLURE_REPORT}/. ./${INPUT_ALLURE_HISTORY}/${REPORT_NUMBER}
 echo "copy allure-report history to /${INPUT_ALLURE_HISTORY}/last-history"
 cp -r ./${INPUT_ALLURE_REPORT}/history/. ./${INPUT_ALLURE_HISTORY}/last-history
